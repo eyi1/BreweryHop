549e5aa831db4da571a98573fe939cecb29c63cc
github username <ej7729@gmail.com>
2019-07-18 18:45:34 -0400

    HTTP get request to URL_API endpoint

--- src/App.js
+++ src/App.js
@@ -2,25 +2,50 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//import axios from 'axios';
+//const API_URL = 'https://api.openbrewerydb.org/breweries';
+
+class App extends React.Component {
+  //function App(){}
+
+  // state = {
+  //   breweries: []
+  // }
+
+  // componentDidMount(){
+  //   const url = `${API_URL}`;
+  //   axios.get(url)
+  //     .then(response => response.data)
+  //     .then((data) => {
+  //       this.setState({
+  //         breweries: data
+  //       })
+  //       console.log(this.state.breweries);     
+  //     })
+  // }
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+          </a>
+          Click here to begin...
+          Welcome to BreweryHop: 
+          Planning a visit to NYC? Check out some of the best breweries here! 
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
