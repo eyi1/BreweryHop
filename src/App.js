c776cf94a80fdab6e814843eabb43c08315988f7
github username <ej7729@gmail.com>
2019-07-21 14:12:53 -0400

    remove react router dom

--- src/App.js
+++ src/App.js
@@ -1,30 +1,8 @@
 import React from 'react';
-//import logo from './logo.svg';
 import './App.css';
 import BreweryContainer from './containers/BreweryContainer'
-import Brewery from './components/Brewery'
-import { Route } from 'react-router-dom';
-//import axios from 'axios';
-//const API_URL = 'https://api.openbrewerydb.org/breweries';
 
 class App extends React.Component {
-  //function App(){}
-
-  // state = {
-  //   breweries: []
-  // }
-
-  // componentDidMount(){
-  //   const url = `${API_URL}`;
-  //   axios.get(url)
-  //     .then(response => response.data)
-  //     .then((data) => {
-  //       this.setState({
-  //         breweries: data
-  //       })
-  //       console.log(this.state.breweries);     
-  //     })
-  // }
 
   render(){
     return (
@@ -41,16 +19,12 @@ class App extends React.Component {
             rel="noopener noreferrer"
           >
           </a>
+          //jumbotron 
           Click here to begin...
           Welcome to BreweryHop: 
           Planning a visit to NYC? Check out some of the best breweries here! 
         </header> */}
-        <header>
-            {/* <Route exact path="/brewery" component={Brewery} />  */}
-        </header>
-
           <BreweryContainer />
-
       </div>
       
     );
