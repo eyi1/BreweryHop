6dee8da2f3568231c08b07f20a4aae6924f870ed
github username <ej7729@gmail.com>
2019-07-21 12:24:32 -0400

    add button with onClick function passed down as props to invoke it

--- src/components/Brewery.js
+++ src/components/Brewery.js
@@ -1,23 +1,69 @@
 import React from 'react'
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+import { Button, ButtonToolbar } from 'react-bootstrap';
 
-export default class Brewery extends React.Component{
+class Brewery extends React.Component{
+  // constructor(props){
+  //   super(props)
+  //   this.state = {
+  //     showComponent: false,
+  //   };
+  //   this.onButtonClick = this.onButtonClick.bind(this);
+  // }
+
+  // onButtonClick = () => {
+  //   this.setState({
+  //     showComponent: true,
+  //   });
+  // }
 
     render(){
         return( 
-            <div className="card">
-             <div className="content">
-              <a className="header">
-                {this.props.brewery.name}
-              </a>
-              <div className="meta">
-                {/* <span className="date">{this.props.brewery.address}</span> */}
-              </div>
-              <div className="description">
-              </div>
-            </div>
+          <div>
+            <Button variant="outline-dark" onClick={() => this.props.onButtonClick(this.props.brewery)}>{this.props.brewery.name}</Button>
+            {/* {this.state.showComponent ?
+                  <Map brewery={this.props.brewery}/> :
+               null
+            } */}
+          
           </div>
-        )
-        
+      //     <Map google={this.props.google} zoom={14}
+      //       // initialCenter={{
+      //       // lat: this.props.brewery.latitude,
+      //       // lng: this.props.brewery.longitude
+      //     // }}
+      //     >
+      //       <Marker onClick={this.onMarkerClick}
+          
+      //       name={'current location'}
+      //       />       
+      //        />
+   
+      //      <InfoWindow onClose={this.onInfoWindowClose}>
+      //         <div>
+      //           {/* <h1>{this.state.selectedPlace.name}</h1> */}
+      //         </div>
+      //      </InfoWindow>
+      //   </Map>
+      // );
+          //   <div className="card">
+          //    <div className="content">
+          //     <a className="header">
+          //       {this.props.lat}
+          //       {this.props.lat}
+          //     </a>
+          //     <div className="meta">
+          //       {/* <span className="date">{this.props.brewery.address}</span> */}
+          //     </div>
+          //     <div className="description">
+          //     </div>
+          //   </div>
+          // </div>
+        )   
     }
 
 }
+
+export default GoogleApiWrapper({
+  apiKey: ("AIzaSyCbzOSFbRLTDIQ8mVJ0LEiurXh6qL_4C-o")
+})(Brewery)
