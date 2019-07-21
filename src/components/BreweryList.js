6474db87d96bbe0e570fb577c3e11d601936bf3c
github username <ej7729@gmail.com>
2019-07-21 12:27:18 -0400

    comment out unneccessary code for now and will keep as pure function to pass down props to Brewery

--- src/components/BreweryList.js
+++ src/components/BreweryList.js
@@ -1,28 +1,50 @@
 import React from 'react';
-//import Brewery from './Brewery';
+import Brewery from './Brewery';
+import { Button, ButtonToolbar } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+import { Link } from 'react-router-dom';
+//import GoogleMapReact from 'google-map-react'
+// import Button from 'react-bootstrap/Button';
+// import ButtonToolbar from 'react-bootstrap/Button';
+//import { withGoogleMap, GoogleMap } from 'react-google-maps';
+// import Map from './components/Map.js'
+// import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+ 
+
 
 export default class BreweryList extends React.Component {
-  
-    clickHandler = () => {
 
-    }
 
     render() {
       return (
-        <div className="ui cards">
-            {/* {this.props.breweriesArray.map(brewery => <Brewery key={brewery.id} brewery={brewery} />)} */}
-        
-          {/* <button onClick={() => this.props.brewery} className="ui primary button">{this.props.brewery.name}</button> */}
-          {this.props.brewery.name}
-          {this.props.brewery.type}
+       // <div className="ui cards">
+       <div>
+            {this.props.breweriesArray.map(brewery => <Brewery key={brewery.id} brewery={brewery} onButtonClick={this.props.onButtonClick} />)}
+            {/* className="ui primary button" */}
+            {/* <ButtonToolbar>
+                <Button variant="outline-light" onClick={this.onButtonClick}>{this.props.brewery.name}</Button>
+                {this.state.showComponent ?
+                  <Brewery brewery={this.props.brewery}/> :
+               null
+               }
+
+</ButtonToolbar> */}
+            
+
+          {/* {this.props.brewery.name} */}
+          {/* {this.props.brewery.type}
           {this.props.brewery.street}
           {this.props.brewery.city}
           {this.props.brewery.state}
           {this.props.brewery.postal_code}
-          <a href={this.props.brewery.website_url}>{this.props.brewery.website_url}</a>
+          <Button variant="outline-dark" href={this.props.brewery.website_url}>Visit Page</Button>
+           */}
+            
+            
+       
+            {/* <Link to="/brewery">{this.props.brewery.name}</Link> */}
+            
         </div>
       )
     }
-  }
-  
-  
+}
