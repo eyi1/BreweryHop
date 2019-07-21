d3778c9a3968374d4d9f9983079e1c532c89efbb
github username <ej7729@gmail.com>
2019-07-21 15:37:20 -0400

    implement google map react

--- src/components/BreweryInfo.js
+++ src/components/BreweryInfo.js
@@ -3,7 +3,10 @@ import GoogleMapReact from 'google-map-react'
 //const AnyReactComponent = ({ text }) => <div>{ text }</div>;
 import Modal from 'react-bootstrap/Modal'
 import { Button, ButtonToolbar } from 'react-bootstrap';
-//import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+//import Location from 'components/Location'
+//import GoogleMapReact from 'google-map-react';
+
 
 export default class BreweryInfo extends Component {
   constructor(props){
@@ -24,10 +27,14 @@ export default class BreweryInfo extends Component {
 }
 
 
-  // static defaultProps = {
-  //   center: { lat: 40.7446790, lng: -73.9485420 },
-  //   zoom: 11
-  //}
+static defaultProps = {
+    center: {
+      lat: 40.678178,
+      lng: -73.944158
+    },
+    zoom: 13
+  };
+
 render() {
   console.log(this.state.brewery)
   let close = () => this.setState({ show: false});
@@ -49,6 +56,7 @@ render() {
               onHide={close}
               container={this}
               aria-labelledby="contained-modal-title"
+              size='xl'
             >
               <Modal.Header closeButton>
                 <Modal.Title id="contained-modal-title">{this.state.brewery.name}</Modal.Title>
@@ -58,6 +66,26 @@ render() {
               {this.props.brewery.city}
               {this.props.brewery.state}
               {this.props.brewery.postal_code}
+
+              <div style={{ height: '100vh', width: '100%' }}>
+              <GoogleMapReact google={this.props.google} 
+            //   style={{width: '100%', height: '100%', position: 'relative'}}
+            //   className="map"
+            //   zoom={14}
+              bootstrapURLKeys={{ key: "AIzaSyCbzOSFbRLTDIQ8mVJ0LEiurXh6qL_4C-o" }}
+                defaultCenter={this.props.center}
+                defaultZoom={this.props.zoom}
+                >
+                {/* <Marker
+                key={place.id}
+                text={place.name}
+                lat={place.geometry.location.lat}
+                lng={place.geometry.location.lng}
+              /> */}
+              </GoogleMapReact>
+            </div>
+
+
               </Modal.Body>
               <Modal.Footer>
                 <Button variant="outline-dark" onClick={close}>Close</Button>
@@ -67,3 +95,7 @@ render() {
     )
   }
 }
+
+// export default GoogleApiWrapper({
+//     apiKey: ("AIzaSyCbzOSFbRLTDIQ8mVJ0LEiurXh6qL_4C-o")
+//   })(Location)
