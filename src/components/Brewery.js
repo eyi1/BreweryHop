192cfcef098f587cf6420bc8221f2a88dbd3896e
github username <ej7729@gmail.com>
2019-07-22 11:54:19 -0400

    remove unused code

--- src/components/Brewery.js
+++ src/components/Brewery.js
@@ -1,52 +1,21 @@
 import React from 'react'
-import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 import { Button, ButtonToolbar } from 'react-bootstrap';
 
-class Brewery extends React.Component{
+export default class Brewery extends React.Component{
 
     render(){
         return( 
           <div>
             <Button variant="outline-dark" onClick={() => this.props.onButtonClick(this.props.brewery)}>{this.props.brewery.name}</Button>
-          
+           
+          {this.props.brewery.type}
+          {this.props.brewery.street}
+          {this.props.brewery.city}
+          {this.props.brewery.state}
+          {this.props.brewery.postal_code} 
+          <Button variant="outline-dark" href={this.props.brewery.website_url}>Visit Page</Button>
           </div>
-      //     <Map google={this.props.google} zoom={14}
-      //       // initialCenter={{
-      //       // lat: this.props.brewery.latitude,
-      //       // lng: this.props.brewery.longitude
-      //     // }}
-      //     >
-      //       <Marker onClick={this.onMarkerClick}
-          
-      //       name={'current location'}
-      //       />       
-      //        />
-   
-      //      <InfoWindow onClose={this.onInfoWindowClose}>
-      //         <div>
-      //           {/* <h1>{this.state.selectedPlace.name}</h1> */}
-      //         </div>
-      //      </InfoWindow>
-      //   </Map>
-      // );
-          //   <div className="card">
-          //    <div className="content">
-          //     <a className="header">
-          //       {this.props.lat}
-          //       {this.props.lat}
-          //     </a>
-          //     <div className="meta">
-          //       {/* <span className="date">{this.props.brewery.address}</span> */}
-          //     </div>
-          //     <div className="description">
-          //     </div>
-          //   </div>
-          // </div>
+
         )   
     }
-
 }
-
-export default GoogleApiWrapper({
-  apiKey: ("AIzaSyCbzOSFbRLTDIQ8mVJ0LEiurXh6qL_4C-o")
-})(Brewery)
