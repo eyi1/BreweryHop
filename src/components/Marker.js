8614b7b45cdfa5e1191e332de6e333dd071fd497
github username <ej7729@gmail.com>
2019-07-22 20:06:20 -0400

    remove console.log

--- src/components/Marker.js
+++ src/components/Marker.js
@@ -2,12 +2,9 @@ import React from 'react';
 import './Marker.css';
 
 const Marker = (props) => {
-    const { color, name } = props;
-    // console.log(props)
     return (
       <div className="marker"
-        style={{ backgroundColor: color}}
-        title={name}
+        style={{ backgroundColor: props.color}}
       />
     );
   };
